6cf041d9f2a3efccdf4125c195fe9c83b6c25572
Nikita Lipatov <6stringsohei@gmail.com>
2025-01-24 17:12:26 +0300

    file logger update

---
+++ internal/infrastructure/persistence/file_logger.go
@@ -0,0 +1,30 @@
+package persistence
+
+import (
+	"fmt"
+	"meatgrinder/internal/application/services"
+	"os"
+	"time"
+)
+
+type FileLogger struct {
+	filePath string
+}
+
+func NewFileLogger(path string) services.ILogger {
+	return &FileLogger{filePath: path}
+}
+
+func (fl *FileLogger) LogEvent(event string) {
+	f, err := os.OpenFile(fl.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("FileLogger open error: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	line := fmt.Sprintf("%s | %s\n", time.Now().Format(time.RFC3339), event)
+	if _, werr := f.WriteString(line); werr != nil {
+		fmt.Printf("FileLogger write error: %v\n", werr)
+	}
+}
